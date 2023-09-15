d3f85a7b169c56ab79a8ef6422eacd712a010dfe
cadeholtry <cvholtry@gmail.com>
2023-09-15 11:49:34 -0700

    Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade as a percentage? ");
+        string userInput = Console.ReadLine();
+        int percentage = int.Parse(userInput);
+        string letter = "F";
+        if (percentage > 89) {
+            letter = "A";
+        }
+        else if (percentage > 79) {
+            letter = "B";
+        }
+        else if (percentage > 69) {
+            letter = "C";
+        }
+        else if (percentage > 59) {
+            letter = "D";
+        }
+        Console.WriteLine($"Your grade is {letter}.");
+        if (percentage > 69) {
+            Console.WriteLine("Congrats! You passed!");
+        }
+        else {
+            Console.WriteLine("You failed! Better luck next time!");
+        }
     }
 }
