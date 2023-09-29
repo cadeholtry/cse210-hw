5e82b7bf190d780d448de94f08e6fe7d850585f8
cadeholtry <cvholtry@gmail.com>
2023-09-28 18:37:58 -0700

    02 Learning

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Lead Programmer";
+        job1._company = "Aggro Crab";
+        job1._startYear = 2019;
+        job1._endYear = 2023;
+        Job job2 = new Job();
+        job2._jobTitle = "Character Designer";
+        job2._company = "Team Cherry";
+        job2._startYear = 2015;
+        job2._endYear = 2019;
+        Resume myResume = new Resume();
+        myResume._name = "Cade Holtry";
+        myResume._jobs.Add(job2);
+        myResume._jobs.Add(job1);
+        myResume.Display();
     }
 }
